4cacee23c9e540510139b964e1c7c9bfeeaa6dae
Andrés David Hernández Muñoz <andres.hernandez@pevaar.com>
2023-02-02 15:20:16 -0500

    more updates

--- UnitTesting/UnitTesting#1/MyClasses/MyClassesTest/FileProcessTest.cs
+++ UnitTesting/UnitTesting#1/MyClasses/MyClassesTest/FileProcessTest.cs
@@ -5,13 +5,31 @@ namespace MyClassesTest
     [TestClass]
     public class FileProcessTest
     {
+        protected string _GoodFileName;
+        private const string BAD_FILE_NAME = @"C:\Windows\Olabb.exe";
+        public TestContext TestContext { get; set; }
+
+        protected void SetGoodFileName()
+        {
+            _GoodFileName = TestContext.Properties["GoodFileName"].ToString();
+            if (_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]",
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
+
         [TestMethod]
         public void FileNameDoesExist()
         {
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+            SetGoodFileName();
+
+            TestContext.WriteLine($"Checking File {_GoodFileName}");
+
+            fromCall = fp.FileExists(_GoodFileName);
 
             Assert.IsTrue(fromCall);
         }
@@ -22,7 +40,9 @@ namespace MyClassesTest
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\Windows\Olabb.exe");
+            TestContext.WriteLine($"Checking File {BAD_FILE_NAME}");
+
+            fromCall = fp.FileExists(BAD_FILE_NAME);
 
             Assert.IsFalse(fromCall);
         }
@@ -33,6 +53,8 @@ namespace MyClassesTest
         {
             FileProcess fp = new FileProcess();
 
+            TestContext.WriteLine("Checking for a null file");
+
             fp.FileExists("");
 
         }
@@ -41,6 +63,8 @@ namespace MyClassesTest
         public void FileNameNullOrEmpty_UsingTryCatch()
         {
             FileProcess fp = new FileProcess();
+
+            TestContext.WriteLine("Checking for a null file");
 
             try
             {
